6f534fcd0051ad19fdeeb977dbffd14d90945f10
lou7523 <ldrsf23042009@gmail.com>
2026-06-30 14:26:17 +0200

    keyboardhandler, backspace, scroll, protecao de overflow, filtro c == 0, shift esquerdo e direito e por fim inicio da gestao de memoria

--- kernel.asm
+++ kernel.asm
@@ -1,35 +1,69 @@
-section .text.boot
-global inicio
+section .text.boot              ;diz ao linker para colocar o .text.boot ficar no inicio do binario final
+global inicio                   ;tornam estas duas funcoes visiveis fora deste ficheiro
+global keyboard_handler         
+extern keyboard_handler_c       ;Diz ao assembler que estas funcoes estao noutro ficheiro
 extern kernel_main
 
-[bits 16]
+[bits 16]                   ;inicio do modo 16bits
+//inicio da funcao inicio
 inicio:
-    cli
+    cli                     ;Clear Interrupt Flag, desliga as interrupcoes para nao ser interrompido por nenhum hardware
 
-    xor ax, ax
-    mov ds, ax
-    mov es, ax
+    xor ax, ax              ;poe AX a 0
+    mov ds, ax              ;copiar o DataSegment (ds) 
+    mov es, ax              ;copiar o ExtraSegment (es)
+    ;temos de fazer isto pois o bootloader assume enderecos a partir de 0x7c00 com segmentos a 0, sem isto
+    ;os enderecos calculados mais a frente iriam ser errados
 
-    o32 lgdt [gdt_descriptor]
-    mov eax, cr0
-    or eax, 1
-    mov cr0, eax
-    jmp dword 0x08:inicio_32bit
+    mov di, 0x9000          ;di vai apontar para o mapa de memoria da BIOS
+    xor ebx, ebx            ;ebx vai ser o marcado de posicao que a BIOS vai usar para saber onde ficou entre uma chamada e a seguinte
 
-[bits 32]
+ler_mapa:
+;inicio da funcao
+
+    mov eax, 0xE820         ;Diz a BIOS que funcao int 0x15 quero
+    mov ecx, 20             ;Intrucao que diz a BIOS que cada bloco tem cerca de 20 bytes
+    mov edx, 0x534D4150     ;A palavra passe fixa SMAP em ASCII, esta e exigida
+    int 0x15                ;Executa o pedido a BIOS
+
+    jc fim_mapa         ;Jump If Carry, verifica se a chamada falhou e salta para fora do ciclo caso aconteca algum erro
+    cmp ebx, 0          ;se ebx for 0, para verificar se ainda a blocos
+    je fim_mapa         ;Jump if Equal caso sim
+
+    add di, 20          ;Avanca-se 20 bytes 
+    jmp ler_mapa        ;volta-se ao inicio da funcao
+
+fim_mapa:
+;Funcao fim mapa
+
+    o32 lgdt [gdt_descriptor]       ; Carrega o GDT na CPU
+    mov eax, cr0                    ; cr0 e um registo para o controlo da CPU, o bit 0 = PE (Protection Enable), e ativa-lo e o que liga o modo 32 bits
+    or eax, 1                       ; vai ler esse bit
+    mov cr0, eax                    ; vai liga-lo sem mexer nos outros bits
+    jmp dword 0x08:inicio_32bit     ; Um salto far jump não só muda o EIP (próxima instrução), como também recarrega CS (Code Segment) para 0x08, 
+                                    ; que é o selector do segmento de código 32-bit na GDT. Isto é o que finaliza a transição,
+                                    ; o CPU só "acredita" mesmo que está em modo protegido depois deste salto.
+
+[bits 32]                           ; A partir daqui o NASM escreve as instrucoes em 32 bits
 inicio_32bit:
-    mov ax, 0x10
+    mov ax, 0x10                    ; 0x10 e o seletor do segmento de dados na minha GDT(segunda entrada)
     mov ds, ax
     mov es, ax
     mov fs, ax
     mov gs, ax
     mov ss, ax
-    mov esp, 0x90000
+    mov esp, 0x90000                ; Define o topo do stack onde push/pop/call/ret vao ser guardados -> 0x90000
 
-    lidt [idt_descriptor]
+    call kernel_main            ; Chama a funcao em C
+    jmp $                       ; ajuda a CPU a ficar num loop infinito
 
-    call kernel_main
-    jmp $
+keyboard_handler:
+    pusha                       ; salva os registos todos
+    call keyboard_handler_c     ; chama a funcao keyboard handler c
+    popa                        ; restaura todos os registos
+    mov al, 0x20                ; Avisa o PIC que terminamos a interrupcao
+    out 0x20, al                ; permite-nos avancar para a prox
+    iret                        ; restaura o EIP, CS e EFLAGS
 
 ; GDT - Global Descriptor Table
 gdt_inicio:
@@ -57,14 +91,3 @@ gdt_fim:
 gdt_descriptor:
     dw gdt_fim - gdt_inicio - 1     ; tamanho do GDT
     dd gdt_inicio                   ; Endereco do GDT
-
-idt_descriptor:
-    dw 0
-    dd 0
-
-; --- Dados ---
-    texto db 'LouOS', 0
-    fonte_off dw 0
-    fonte_seg dw 0
-    cursor_x dw 0
-    cursor_y dw 0
