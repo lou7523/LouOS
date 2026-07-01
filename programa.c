4bc6ec18d40667456e6fafb96b6a9b8436f7e7b3
lou7523 <ldrsf23042009@gmail.com>
2026-07-01 13:08:31 +0200

    O que foi feito: programa.c arranjei o endereco fixo de memória
    Bugs:
    GCC usou como output o endereco de Linux
    que e demasiado alto para o nosso setup de
    memoria fixa, logo criei um programa.ld
    para meter o programa a 0x400000 (4MB),
    em cima da kernel e do bitmap

---
+++ programa.c
@@ -0,0 +1,13 @@
+void _start() {
+    char* video = (char*) 0x8B000;
+
+    char* msg = "Ola Mundo!";
+    int i = 0;
+    while (msg[i] != 0) {
+        video[(80 + i) * 2] = msg[i];
+        video[(80 + i) * 2 + 1] = 0x0A;
+        i++;
+    }
+
+    while(1);
+}
